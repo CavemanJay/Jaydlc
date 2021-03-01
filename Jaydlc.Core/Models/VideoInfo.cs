c7a68819eef7c175fd542877b4ad38d3ca5f7eca
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2021-02-28 23:56:45 -0500

    Fix json deserialization of youtube-dl output

--- Jaydlc.Core/Models/VideoInfo.cs
+++ Jaydlc.Core/Models/VideoInfo.cs
@@ -5,75 +5,92 @@ namespace Jaydlc.Core.Models
 {
     public record VideoInfo
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
+        [JsonPropertyName("id")]
+        public string Id { get; init; }
 
-        public List<Thumbnail> Thumbnails { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; init; }
 
-        // public List<Format> formats { get; set; }
-        public string Description { get; set; }
+        [JsonPropertyName("thumbnails")]
+        public List<Thumbnail> Thumbnails { get; init; }
+
+        // public List<Format> formats { get; init; }
+        [JsonPropertyName("description")]
+        public string Description { get; init; }
 
         [JsonPropertyName("upload_date")]
-        public string UploadDate { get; set; }
+        public string UploadDate { get; init; }
 
-        // public string uploader { get; set; }
-        // public string uploader_id { get; set; }
-        // public string uploader_url { get; set; }
-        // public string channel_id { get; set; }
-        // public string channel_url { get; set; }
+        // public string uploader { get; init; }
+        // public string uploader_id { get; init; }
+        // public string uploader_url { get; init; }
+        // public string channel_id { get; init; }
+        // public string channel_url { get; init; }
         [JsonPropertyName("duration")]
-        public double DurationInSeconds { get; set; }
+        public double DurationInSeconds { get; init; }
 
-        public int view_count { get; set; }
-        // public double average_rating { get; set; }
-        // public int age_limit { get; set; }
+        public int view_count { get; init; }
+        // public double average_rating { get; init; }
+        // public int age_limit { get; init; }
 
         [JsonPropertyName("webpage_url")]
-        public string VideoLink { get; set; }
-        // public List<string> categories { get; set; }
-        // public List<object> tags { get; set; }
-        // public object is_live { get; set; }
-        // public int like_count { get; set; }
-        // public int dislike_count { get; set; }
-        // public object channel { get; set; }
-        // public string extractor { get; set; }
-        // public string webpage_url_basename { get; set; }
-        // public string extractor_key { get; set; }
-        // public int n_entries { get; set; }
-        // public string playlist { get; set; }
+        public string VideoLink { get; init; }
+        // public List<string> categories { get; init; }
+        // public List<object> tags { get; init; }
+        // public object is_live { get; init; }
+        // public int like_count { get; init; }
+        // public int dislike_count { get; init; }
+        // public object channel { get; init; }
+        // public string extractor { get; init; }
+        // public string webpage_url_basename { get; init; }
+        // public string extractor_key { get; init; }
+        // public int n_entries { get; init; }
+        // public string playlist { get; init; }
 
         [JsonPropertyName("playlist_id")]
-        public string PlaylistId { get; set; }
+        public string PlaylistId { get; init; }
 
         [JsonPropertyName("playlist_title")]
-        public string PlaylistTitle { get; set; }
+        public string PlaylistTitle { get; init; }
 
-        // public string playlist_uploader { get; set; }
-        // public string playlist_uploader_id { get; set; }
-        public int Playlist_index { get; set; }
-        public string Thumbnail { get; set; }
+        // public string playlist_uploader { get; init; }
+        // public string playlist_uploader_id { get; init; }
+        public int Playlist_index { get; init; }
 
-        public string Display_id { get; set; }
+        [JsonPropertyName("thumbnail")]
+        public string Thumbnail { get; init; }
 
-        // public int asr { get; set; }
-        // public object filesize { get; set; }
-        // public string format_id { get; set; }
-        // public string format_note { get; set; }
-        public int Fps { get; set; }
-        public int Height { get; set; }
+        // public string Display_id { get; init; }
 
-        public int Quality { get; set; }
+        // public int asr { get; init; }
+        // public object filesize { get; init; }
+        // public string format_id { get; init; }
+        // public string format_note { get; init; }
 
-        // public double tbr { get; set; }
-        public string url { get; set; }
+        [JsonPropertyName("fps")]
+        public int Fps { get; init; }
 
-        // public int width { get; set; }
-        // public string ext { get; set; }
-        // public string vcodec { get; set; }
-        // public string acodec { get; set; }
-        // public string format { get; set; }
-        public string protocol { get; set; }
-        public string FullTitle { get; set; }
-        public string _filename { get; set; }
+        // [JsonPropertyName("height")]
+        // public int Height { get; init; }
+
+        [JsonPropertyName("height")]
+        public int Quality { get; init; }
+
+        // public double tbr { get; init; }
+        // [JsonPropertyName("url")]
+        // public string Url { get; init; }
+
+        // public int width { get; init; }
+        // public string ext { get; init; }
+        // public string vcodec { get; init; }
+        // public string acodec { get; init; }
+        // public string format { get; init; }
+        // public string protocol { get; init; }
+
+        [JsonPropertyName("fulltitle")]
+        public string FullTitle { get; init; }
+
+        [JsonPropertyName("_filename")]
+        public string FileName { get; init; }
     }
 }
